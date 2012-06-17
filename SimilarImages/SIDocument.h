03069df3c6a0942b46ff5f53e1b406abf163179c
Dorian Johnson <2012@dorianj.net>
2012-06-17 18:22:57 -0500

    Initial Commit

---
+++ SimilarImages/SIDocument.h
@@ -0,0 +1,13 @@
+//
+//  SIDocument.h
+//  SimilarImages
+//
+//  Created by Dorian Johnson on 2012/6/17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SIDocument : NSDocument
+
+@end
---
+++ SimilarImages/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SimilarImages
+//
+//  Created by Dorian Johnson on 2012/6/17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
